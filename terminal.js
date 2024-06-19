0259df2ca39056444ab61d5a5dc5a643b9841e1d
vjauxysbrynxx <90746160+vjauxysbrynxx@users.noreply.github.com>
2024-06-20 01:23:33 +0800

    kulang pa content

--- terminal.js
+++ terminal.js
@@ -3,15 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const terminalOutput = document.getElementById("terminal-output");
 
     const commands = {
-        help: "Available commands: help, about, contact, clear, portfolio, blog",
-        about: "I Benz Vrianne Beleber, a web developer with a passion for creating interactive web experiences.",
-        contact: "You can reach me at myemail@example.com",
-        portfolio: () => {
-            window.location.href = "https://your-portfolio-link.com";
+        help: "Available commands: help, about, contact, cv, projects, and clear.", typeWriter,
+        about: "I am Benz Vrianne Beleber, a web developer with a passion for creating interactive web experiences.",
+        contact: "You can reach me at bpbeleber@up.edu.ph",
+        cv: () => {
+            window.location.href = "\Beleber, Benz Vrianne CV.pdf";
         },
-        blog: () => {
-            window.location.href = "https://your-blog-link.com";
-        },
+
+        projects: "",
+
         clear: () => {
             terminalOutput.innerHTML = "";
         }
@@ -34,15 +34,32 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (typeof commands[input] === "function") {
                     commands[input]();
                 } else {
-                    commandOutput.textContent = commands[input];
+                    typeWriter(commands[input], commandOutput);
                     terminalOutput.appendChild(commandOutput);
                 }
             } else {
-                commandOutput.textContent = `'${input}' is not recognized as a command. Type 'help' for a list of commands.`;
+                typeWriter(`'${input}' is not recognized as a command. Type 'help' for a list of commands.`, commandOutput);
                 terminalOutput.appendChild(commandOutput);
             }
 
+            const blankLine = document.createElement("div");
+            blankLine.classList.add("output-line");
+            blankLine.innerHTML = "&nbsp;"; //non breaking space
+            terminalOutput.appendChild(blankLine);
             terminalOutput.scrollTop = terminalOutput.scrollHeight;
         }
     });
+
+    function typeWriter(text, element) {
+        let i = 0;
+        function typingEffect() {
+            if (i < text.length) {
+                element.innerHTML += text.charAt(i);
+                i++;
+                setTimeout(typingEffect, 10);
+            }
+        }
+        typingEffect();
+    }
+
 });
